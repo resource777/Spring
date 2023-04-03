62b2142fc6c2dc6694921305e757b0c64a52cc85
Jaewon Kim <resource777@naver.com>
2023-04-03 11:37:14 +0900

    새로운 구조와 할인 정책 적용

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -24,6 +24,8 @@ public class AppConfig {
     }
 
     public DiscountPolicy discountPolicy() {
-        return new FixDiscountPolicy();
+        //return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
+
     }
 }
